8a6ed50ef8c6d96724c9100821c45ad8a0aca710
SleepyDan <97019199+SleepyDaniel@users.noreply.github.com>
2023-07-25 13:40:30 +0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     `password` varchar(200) NOT NULL, 
     `verified` boolean, 
     `phone` varchar(200), 
-    `discordID` varchar(20), 
+    `discordID` varchar(20)
     
     PRIMARY KEY (`ID`) 
 );
